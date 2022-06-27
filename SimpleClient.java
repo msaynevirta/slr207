55e41cfe2fa1507b0a3b2919109e22d7b1ab4eb7
Markus Säynevirta <25539916+msaynevirta@users.noreply.github.com>
2022-06-28 01:49:59 +0200

    cleaner implementation until start of shuffle

--- SimpleClient.java
+++ SimpleClient.java
@@ -7,60 +7,56 @@ public class SimpleClient {
     public static void main(String[] args) {
 
         // Server Host
-        final String username = "msaynevi-21";
-        final String serverHost[] = { "tp-1a226-07.enst.fr",
-                                      "tp-1a226-08.enst.fr",
-                                      "tp-1a226-09.enst.fr" };
+        final String username = "make";
+        //final String username = "msaynevi-21";
+        final String serverHost[] = { "localhost" };
+        // final String serverHost[] = { "tp-1a226-07.enst.fr",
+        //                               "tp-1a226-08.enst.fr",
+        //                               "tp-1a226-11.enst.fr" };
         final Integer nSplits = serverHost.length;
 
-        List<Socket> socketsOfClient = new ArrayList<Socket>();
-        BufferedOutputStream bos = null;
-
         try {
-            String sourceDir = "splits/";
-            String destDir = "/tmp/" + username + "/";
+            String parentDir = "/tmp/" + username + "/";
+            String filePath = "splits/";
 
-            // Open sockets
-            for(String serverAddress : serverHost) {
-                socketsOfClient.add(new Socket(serverAddress, 3419));
-            }
-
-            for(Integer i = 0; i < nSplits; i++) {
+            for(int i = 0; i < nSplits; i++) {
+                Socket socket = new Socket(serverHost[i], 3419);
                 String filename = "s" + Integer.toString(i) + ".txt";
 
                 // Create output stream at the client (to send data to the server)
-                bos = new BufferedOutputStream(socketsOfClient.get(i).getOutputStream());
-                DataInputStream dis = new DataInputStream(new FileInputStream(sourceDir + filename));
+                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+                DataInputStream dis = new DataInputStream(new FileInputStream(filePath + filename));
 
-                // Transfer files and server address list
+                // Write the filepath and filename on the server
+                dos.writeUTF(parentDir);
+                dos.writeUTF(filename);
 
-                try (DataOutputStream dos = new DataOutputStream(bos)) {
-                    // Write the filepath and filename on the server
-                    dos.writeUTF(destDir);
-                    dos.writeUTF(filename);
-
-                    // Write serverHost array
-                    dos.writeInt(serverHost.length);
-                    for(int j = 0; j < serverHost.length; j++) {
-                        dos.writeUTF(serverHost[j]);
-                    }
-                        
-                    // Write the file
-                    while(dis.available() > 0)
-                        dos.write(dis.readByte());
-
-                    dos.close();
-                    dis.close();
-                    
+                System.out.println("wrote filenames to " + serverHost[i]);
+                
+                // Write serverHost array
+                dos.writeInt(serverHost.length);
+                for (int j = 0; j < serverHost.length; j++) {
+                    dos.writeUTF(serverHost[j]);
                 }
-                bos.close();
+
+                // Write the file
+                while(dis.available() > 0)
+                    dos.write(dis.readByte());
+
+                System.out.println("wrote file s" + Integer.toString(i) + ".txt to " + serverHost[i]);
+
+                dis.close();
+                dos.close();
+                socket.close();
             }
 
-            for(Socket cs : socketsOfClient) {
-                bos = new BufferedOutputStream(cs.getOutputStream());
-                bos.write("SHUFFLE".getBytes());
-                bos.close();
-                cs.close();
+            // Tell the servers that 
+            for (String address : serverHost) {
+                Socket socket = new Socket(address, 3419);
+                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+                dos.writeUTF("SHUFFLE");
+                dos.close();
+                socket.close();
             }
 
         } catch (UnknownHostException e) {
--- SimpleServer.java
+++ SimpleServer.java
@@ -1,22 +1,18 @@
 import java.io.*;
-import java.nio.file.FileSystems;
-//import java.nio.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.net.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class SimpleServer {
+
     public static void main(String args[]) {
         ServerSocket listener = null;
-        //DataOutputStream dos = null;
         Socket socketOfServer = null;
+
+        // Try to open a server socket on port 9999
+        // Note that we can't choose a port less than 1023 if we are not
+        // privileged users (root)
 
         try {
             listener = new ServerSocket(3419);
@@ -29,161 +25,69 @@ public class SimpleServer {
             System.out.println("Server is waiting to accept user...");
 
             // Accept client connection request
-            // Get new Socket at Server.    
+            // Get new Socket at Server.
             socketOfServer = listener.accept();
             System.out.println("Accept a client!");
 
             List<String> serverHost = new ArrayList<String>();
-            
             String parentDir = null;
-            String filename;
+            String splitfile = null;
 
-            /* -------- READING SPLITS -------- */
-            BufferedInputStream bis = new BufferedInputStream(socketOfServer.getInputStream());
-            
-            try (DataInputStream dis = new DataInputStream(bis)) {
-                // Read the parent directory on the server
-                parentDir = dis.readUTF();
+            /* --------------- SPLITS & SERVER ADDRESSES --------------- */
+            DataInputStream dis = new DataInputStream(bis);
+            DataOutputStream dos = new DataOutputStream(new FileOutputStream(parentDir + splitfile));
 
-                // Create the directories
-                File parent = new File(parentDir);
-                parent.mkdirs();
+            parentDir = dis.readUTF();
+            splitfile = dis.readUTF();
 
-                // Read the filename for the server
-                filename = dis.readUTF();
+            Integer serverHostLen = dis.readInt();
+            for(int j = 0; j < serverHostLen; j++)
+                serverHost.add(dis.readUTF());
 
-                // Read serverHost array
-                Integer serverHostLen = dis.readInt();
-                for(Integer j = 0; j < serverHostLen; j++)
-                    serverHost.add(dis.readUTF());
+            while(dis.available() > 0)
+                dos.write(dis.readByte());
 
-                File f = new File(parentDir + filename);
+            System.out.println("paska");
+            //dis.close();
+            dos.close();
+            dis.close();
 
-                // Read the file
-                Files.copy(dis, f.toPath());
+            /* --------------- SHUFFLE --------------- */
+            System.out.println("Server is waiting to start SHUFFLE...");
 
-                dis.close();
-            }
+            // Accept client connection request
+            // Get new Socket at Server.
+            socketOfServer = listener.accept();
+            System.out.println("Accept a client!");
 
-            if(bis.readAllBytes().equals("SHUFFLE".getBytes())) {
-                System.out.println("Starting Shuffle");
-            }
-
-            bis.close();
-
-            System.out.println("Wrote to " + parentDir + filename);
-
-            
-
-            // Receive sync signal for suffle?
-
-            // Open sockets and output streams
-
+            /* --------------- SERVER CONNECTIONS --------------- */
             List<Socket> serverSockets = new ArrayList<Socket>();
-            List<BufferedWriter> serverWriters = new ArrayList<BufferedWriter>();
-
-            for(String serverAddress : serverHost) {
-                Socket s = new Socket(serverAddress, 3419);
-                serverSockets.add(s);
-                serverWriters.add(new BufferedWriter(new OutputStreamWriter(s.getOutputStream())));
-            }
-
-            /* -------- SHUFFLE -------- */
-            Integer nServers = serverHost.size();
-
-            Listener listenerThread = new Listener(nServers);
-            listenerThread.start();
-
-            BufferedReader bin = null;
             try {
-                
-                bin = new BufferedReader(new FileReader(parentDir + filename));
-                String read = null;
-                while ((read = bin.readLine()) != null) {
-                    String[] splited = read.split("\\s+");
-    
-                    for (String word : splited) {
-                        Integer hash = word.hashCode() & 0x7fffffff; // make it positive
-                        serverWriters.get(hash % nServers).write(word);
-                    }
-                }
-                for(BufferedWriter s : serverWriters) {
-                    s.write(-1);
+                for(String address : serverHost) {
+                    serverSockets.add(new Socket(address, 3419));
                 }
             } catch (IOException e) {
                 System.out.println(e);
                 e.printStackTrace();
-            } finally {
-                try {
-                    bin.close();
-                } catch (Exception e) {
-                }
             }
 
+            // Open buffered input and output streams
+
+            bis.close();
+            socketOfServer.close();
+
+            
 
         } catch (IOException e) {
             System.out.println(e);
             e.printStackTrace();
         }
-        
         System.out.println("Server stopped!");
     }
 }
 
 class Listener extends Thread {
-    Integer serverCount;
-
-    public Listener(Integer nServers) {
-        serverCount = nServers;
-    }
-
-    public void run(String args[]) {
-        ServerSocket listener = null;
-        //DataOutputStream dos = null;
-        Socket socketOfServer = null;
-
-        try {
-            listener = new ServerSocket(3419);
-        } catch (IOException e) {
-            System.out.println(e);
-            System.exit(1);
-        }
-        
-        try {
-            System.out.println("Server is waiting to accept user...");
-
-            // Accept client connection request
-            // Get new Socket at Server.    
-            socketOfServer = listener.accept();
-
-            // Put the word counts into a hashmap, needs separate thread (after shuffle)
-            Map<String, Integer> hashMap = new HashMap<>();
-
-            BufferedReader bin = new BufferedReader(new InputStreamReader(socketOfServer.getInputStream()));
-            try {
-                String word = null;
-                Integer finished = 0;
-                do {
-                    word = bin.readLine();
-                    if(word.isEmpty()) {
-                        finished++;
-                    }
-                } while(finished.equals(serverCount));
-                
-            } catch (IOException e) {
-                System.out.println(e);
-                e.printStackTrace();
-            } finally {
-                try {
-                    bin.close();
-                } catch (Exception e) {
-                }
-            }
-            System.out.println("Server stopped!");
-            
-        } catch (IOException e) {
-            System.out.println(e);
-            e.printStackTrace();
-        }
+    public void run() {
+        System.out.println("Started listener");
     }
 }
